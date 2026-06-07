6aa96f9f2da8b077ab7a60debe476028f8fccbd2
aratisutar774-web <aratisutar774@gmail.com>
2026-06-07 20:56:21 +0200

    feat: add main entrypoint

---
+++ main.py
@@ -0,0 +1,11 @@
+import uvicorn
+from config.settings import settings
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app.api.server:app",
+        host=settings.api_host,
+        port=settings.api_port,
+        reload=settings.api_debug,
+        log_level="info",
+    )
